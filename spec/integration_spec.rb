2b6e057cef846370792b6255aed2dc7bab4b0d51
Tracie Weitzman <tlweitzman@gmail.com>
2016-12-16 08:40:32 -0800

    pass integration spec for adding new stores

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -8,3 +8,12 @@ describe('the add brand route', {:type => :feature}) do
     expect(page).to have_content('Patagonia')
   end
 end
+
+describe('the add shoe store route', {:type => :feature}) do
+  it('will add a new store') do
+    visit('/')
+    fill_in('store_name', :with => 'fancy shoes')
+    click_button('Add Shoe Store')
+    expect(page).to have_content('Fancy Shoes')
+  end
+end
