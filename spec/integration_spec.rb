8303a7b4ffe753d8fc1d620240997eb3e6e59d82
Tracie Weitzman <tlweitzman@gmail.com>
2016-12-16 08:32:44 -0800

    pass integration spec for adding new brands

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,1 +1,10 @@
 require('spec_helper')
+
+describe('the add brand route', {:type => :feature}) do
+  it('will add a new brand') do
+    visit('/')
+    fill_in('brand_name', :with => 'patagonia')
+    click_button('Add Brand')
+    expect(page).to have_content('Patagonia')
+  end
+end
