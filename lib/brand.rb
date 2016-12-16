dfb0452e7c271422c17d4e39cba4b3ad055e7086
Tracie Weitzman <tlweitzman@gmail.com>
2016-12-16 08:12:05 -0800

    pass brand class validation spec

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,7 +1,7 @@
 class Brand < ActiveRecord::Base
   has_and_belongs_to_many(:stores)
-#   validates(:name, :presence => true, :uniqueness => {:case_sensitive => false})
-#   before_save(:capitalize_brand)
+  validates(:name, :presence => true, :uniqueness => {:case_sensitive => false})
+  # before_save(:capitalize_brand)
 #
 #
 # private
--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -3,8 +3,8 @@ require('spec_helper')
 describe(Brand) do
   it { should have_and_belong_to_many(:stores)}
 
-  # it { should validate_presence_of(:name)}
-  #
+  it { should validate_presence_of(:name)}
+
   # it { should validate_uniqueness_of(:name)}
   #
   # it('capitalizes all words of a brand name') do
