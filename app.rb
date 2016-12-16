88f1cdd7c1d8b28b1f90b95c7afa7c5767050b4d
Tracie Weitzman <tlweitzman@gmail.com>
2016-12-16 08:30:47 -0800

    add routing for adding Brands and individual Brand pages in front end

--- app.rb
+++ app.rb
@@ -3,3 +3,25 @@ Bundler.require(:default)
 require('pry')
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  @brands = Brand.all()
+  # @stores = Stores.all()
+  erb(:index)
+end
+
+post('/brands') do
+  @new_brand = Brand.new(:name => params['brand_name'])
+  if @new_brand.save()
+    redirect('/brands/'.concat(@new_brand.id().to_s()))
+  else
+    @brand = @new_brand
+    erb(:errors)
+  end
+end
+
+get('/brands/:id') do
+  # @stores = Store.all()
+  @brand = Brand.find(params['id'].to_i())
+  erb(:brand)
+end
