077de078501baf157d6e503dcb38bed4f2f7939d
Tracie Weitzman <tlweitzman@gmail.com>
2016-12-16 08:15:23 -0800

    pass validation specs for Store class

--- lib/store.rb
+++ lib/store.rb
@@ -1,6 +1,6 @@
 class Store < ActiveRecord::Base
   has_and_belongs_to_many(:brands)
-#   validates(:name, :presence => true, :uniqueness => {:case_sensitive => false})
+  validates(:name, :presence => true, :uniqueness => {:case_sensitive => false})
 #   before_save(:capitalize_store)
 #
 #
--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -2,11 +2,11 @@ require('spec_helper')
 
 describe(Store) do
   it { should have_and_belong_to_many(:brands)}
-  #
-#   # it { should validate_presence_of(:name)}
-#   #
-#   # it { should validate_uniqueness_of(:name)}
-#   #
+
+  it { should validate_presence_of(:name)}
+
+  it { should validate_uniqueness_of(:name)}
+
 #   # it('capitalizes all words of a store name') do
 #   #   test_store = Store.create({:name => 'birkenstock store'})
 #   #   expect(test_store.name()).to(eq('Birkenstock Store'))
